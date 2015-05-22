41f0e218222359962ce26dd901bfc2d254a9451b
Lane <zhanglintc623@gmail.com>
2015-05-22 18:33:12 +0800

    batch file version upload

---
+++ SetWallpaper.cmd
@@ -0,0 +1,20 @@
+@echo off
+
+REM this script's path, to support remote call
+set target=%~dp0
+
+REM download image from website & rename
+%target%\wget -P %target% http://area.sinaapp.com/bingImg?daysAgo=0
+rename %target%\*.jpg TranscodedWallpaper.jpg
+
+REM convert jpg to bmp
+%target%\jpg2bmp.py %target%\TranscodedWallpaper.jpg
+
+REM copy file to wallpaper folder & set as wallpaper
+REM NOTE: there's no slash between %target% and *.bmp file -> %target%TranscodedWallpaper.bmp
+xcopy /y %target%TranscodedWallpaper.bmp C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Themes\
+%target%\SetWallpaper C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Themes\TranscodedWallpaper.bmp
+
+REM remove temp image files
+del /q %target%\*.bmp %target%\*.jpg
+
