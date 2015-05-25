4a0382c1d6c658b2a8dbfff60e96548cad1dc78a
Lane <zhanglintc623@gmail.com>
2015-05-25 16:14:46 +0800

    use SetWallpaper.exe as default converter

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # SetWallpaper
 Download today's Bing image and set it as wallpaper.
+
+Please double click "SetWallpaper.cmd" to use.
--- SetWallpaper.cmd
+++ SetWallpaper.cmd
@@ -8,7 +8,7 @@ REM download image from website & rename
 rename %target%\*.jpg TranscodedWallpaper.jpg
 
 REM convert jpg to bmp
-%target%\jpg2bmp.py %target%\TranscodedWallpaper.jpg
+%target%\jpg2bmp.exe %target%\TranscodedWallpaper.jpg
 
 REM copy file to wallpaper folder & set as wallpaper
 REM NOTE: there's no slash between %target% and *.bmp file -> %target%TranscodedWallpaper.bmp
